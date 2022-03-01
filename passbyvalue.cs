b5a94bed1aa7ccf4df1b14c9fe691e34c3e859c2
Kuche Divyashree <100555223+kuchedivyashree@users.noreply.github.com>
2022-03-01 22:31:47 +0530

    passbyvalue.cs

---
+++ passbyvalue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+
+
+namespace ConsoleApp2
+{
+    internal class passing
+    {
+        public void doSwap(int a, int b)
+        {
+            int temp;
+            temp = a;
+             a = b;
+             b = temp;
+            Console.WriteLine("after swapping in method: a = {0}, b = {1}",a,b);
+         }
+    
+         static void Main(string[] args)
+         {
+        passing n = new passing();
+        int x = 1;
+        int y = 2;
+
+        Console.WriteLine("before swapping:x={0},y={1}", x, y);
+        n.doSwap(x, y);
+        Console.WriteLine("after swapping:x={0},y={1}", x, y);
+        Console.ReadLine();
+
+
+         }
+
+
+
+        
+    }
+}
