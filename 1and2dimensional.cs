afa652b810406d3c5f2c61f6f4dbc71b07f23fa1
Kuche Divyashree <100555223+kuchedivyashree@users.noreply.github.com>
2022-03-01 22:30:31 +0530

    1and2dimensional.cs

---
+++ 1and2dimensional.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class1
+    {
+        public static void Main()
+        {
+            string[] months;
+            months = new string[] { "jan", "feb", "mar", "apr", "may" };
+            foreach (string month in months)
+                Console.WriteLine(months);
+            string[] dotnet = { "c#", "asp", "add" };
+            for(int i=0;i<dotnet.Length;i++)
+            {
+                Console.WriteLine(dotnet[i]);
+            }
+            foreach(string i in dotnet)
+            {
+                Console.WriteLine(i);
+            }
+            Array.Sort(dotnet);
+            foreach(string i in dotnet)
+            {
+                Console.WriteLine(i);
+            }
+            int[] myNumbers = { 5, 1, 8, 9 };
+            Console.WriteLine(myNumbers.Max());
+            Console.WriteLine(myNumbers.Min());
+            Console.WriteLine(myNumbers.Sum());
+            Console.ReadLine();
+        }
+
+
+    }
+}
