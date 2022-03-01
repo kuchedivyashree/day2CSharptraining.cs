5bf00be77c3e61940e7ae2a66f62ff5e832dec46
Kuche Divyashree <100555223+kuchedivyashree@users.noreply.github.com>
2022-03-01 22:32:37 +0530

    passbyreference.cs

---
+++ passbyreference.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    internal class passbyreference
+    {
+
+        void doSwap(ref int x, ref int y)
+        {
+
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+        }
+        static void Main(string[] args)
+        {
+            passbyreference n = new passbyreference();
+            int a = 1;
+            int b = 2;
+
+            Console.WriteLine("before swapping :a={0},b={1}", a, b);
+            n.doSwap(ref a, ref b);
+            Console.WriteLine("after swapping: a ={0},b ={1}", a, b);
+            Console.ReadLine();
+        }
+            
+    }
+}
